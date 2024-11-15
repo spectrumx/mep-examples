63254caf0b2ddd5cd80e8ee6fc4e320ad13e9d25
Nicholas Rainville <nira6794@colorado.edu>
2024-11-14 19:09:17 -0700

    Update to match repository path change

--- scripts/start_rx.bash
+++ scripts/start_rx.bash
@@ -1,2 +1,2 @@
 #!/bin/bash
-ssh root@192.168.20.100 "source /usr/local/share/pynq-venv/bin/activate && BOARD=RFSoC4x2 XILINX_XRT=/usr python /home/xilinx/git/rfsoc_qsfp_10g/boards/RFSoC4x2/rfsoc_qsfp_offload/scripts/start_capture_rx.py ${@}"
+ssh root@192.168.20.100 "source /usr/local/share/pynq-venv/bin/activate && BOARD=RFSoC4x2 XILINX_XRT=/usr python /opt/git/rfsoc_qsfp_10g/boards/RFSoC4x2/rfsoc_qsfp_offload/scripts/start_capture_rx.py ${@}"
