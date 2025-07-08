9c72a548011b8be91a119552318a423688148003
Nicholas Rainville <nicholas.rainville@colorado.edu>
2025-07-07 22:01:24 -0600

    force ntp update on rfsoc

---
+++ scripts/rfsoc_update_ntp.bash
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Safely quote all passed arguments
+REMOTE_ARGS=$(printf '%q ' "$@")
+
+ssh -t root@192.168.20.100 "ntpdate -s 192.168.20.1 && systemctl restart ntp && echo 'NTP update completed. Current system time:' && date" 
