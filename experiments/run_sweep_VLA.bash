fd77d59519b9056c58c6c3b01adaa352fa92a5c1
John Marino <john.marino@colorado.edu>
2025-07-07 18:21:28 -0700

    Add Jetson Time Sync, Tuner Selection

--- experiments/run_sweep_VLA.bash
+++ experiments/run_sweep_VLA.bash
@@ -1,9 +1,42 @@
 #!/bin/bash
 
+# === NOTES === #
+# A script to start other scripts needed to collect data for the VLA experiment, each in separate screen sessions
+#
+#    Sets the Jetson time to the remote computer's time (Or else jetson time is 1970 which causes problems) [jetson_time]
+#    Starts the RF SOC configuration: "./start_rfsoc_rx.bash -c A B -r" [rfsoc_rx]
+#    Starts the Tuner Sweep: "python3 start_mep_rx.py -f1 7125 -s 10 -d 6000 -t VALON" [mep_rx]
+#    Starts the DigitalRF recording: "python3 start_rec.py -c A -r 1" [recorder_digitalrf]
+#    (disabled) Starts gnuradio: "gnuradio-companion" [gnuradio]
+#    Starts the jetson power/temp monitor: "sudo python /usr/share/jetsonpowergui/__main__.py" [jetsonpowergui]
+#
+
 # ===== COMMON ===== #
+TUNER="VALON" #VALON, LMX2820, or TEST
 WORKDIR="/opt/mep-examples/scripts"
+
+# ===== REPORT ===== #
 mkdir -p /tmp/mep_screens
 echo "Starting Screen Sessions"
+
+# ===== SCREEN SESSION: Jetson Time ===== #
+SESSION0="jetson_time"
+CMD0="./system_time_sync.bash"
+
+screen -S $SESSION0 -X quit 2>/dev/null
+cat > /tmp/mep_screens/$SESSION0.sh <<EOF
+#!/usr/bin/env bash -l
+source /opt/radioconda/etc/profile.d/conda.sh
+conda activate base
+cd "$WORKDIR"
+echo "Running: $CMD0"
+$CMD1
+exec bash
+EOF
+
+chmod +x /tmp/mep_screens/$SESSION0.sh
+screen -dmS $SESSION0 bash /tmp/mep_screens/$SESSION0.sh
+echo "... screen -xS $SESSION0"
 
 # ===== SCREEN SESSION: rfsoc_rx ===== #
 SESSION1="rfsoc_rx"
@@ -26,7 +59,7 @@ echo "... screen -xS $SESSION1"
 
 # ===== SCREEN SESSION: mep_rx ===== #
 SESSION2="mep_rx"
-CMD2="python3 start_mep_rx.py -f1 7125 -s 10 -d 6000 -t VALON"
+CMD2="python3 start_mep_rx.py -f1 7000 -f2 8500 -s 10 -d 10 -t $TUNER"
 
 screen -S $SESSION2 -X quit 2>/dev/null
 cat > /tmp/mep_screens/$SESSION2.sh <<EOF
@@ -63,23 +96,23 @@ screen -dmS $SESSION3 bash /tmp/mep_screens/$SESSION3.sh
 echo "... screen -xS $SESSION3"
 
 # ===== SCREEN SESSION: gnuradio ===== #
-SESSION4="gnuradio"
-CMD4="gnuradio-companion"
-
-screen -S $SESSION4 -X quit 2>/dev/null
-cat > /tmp/mep_screens/$SESSION4.sh <<EOF
-#!/usr/bin/env bash -l
-source /opt/radioconda/etc/profile.d/conda.sh
-conda activate base
-cd "$WORKDIR"
-echo "Running: $CMD4"
-$CMD4
-exec bash
-EOF
-
-chmod +x /tmp/mep_screens/$SESSION4.sh
-screen -dmS $SESSION4 bash /tmp/mep_screens/$SESSION4.sh
-echo "... screen -xS $SESSION4"
+#SESSION4="gnuradio"
+#CMD4="gnuradio-companion"
+#
+#screen -S $SESSION4 -X quit 2>/dev/null
+#cat > /tmp/mep_screens/$SESSION4.sh <<EOF
+##!/usr/bin/env bash -l
+#source /opt/radioconda/etc/profile.d/conda.sh
+#conda activate base
+#cd "$WORKDIR"
+#echo "Running: $CMD4"
+#$CMD4
+#exec bash
+#EOF
+#
+#chmod +x /tmp/mep_screens/$SESSION4.sh
+#screen -dmS $SESSION4 bash /tmp/mep_screens/$SESSION4.sh
+#echo "... screen -xS $SESSION4"
 
 # ===== SCREEN SESSION: Jetson Power GUI ===== #
 SESSION5="jetsonpowergui"
